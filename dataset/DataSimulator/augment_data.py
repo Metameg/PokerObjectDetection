6899f459b2a16db5250eb15b494915e385e58e7e
Metameg <ametzger08@gmail.com>
2025-04-08 12:19:13 -0500

    added remove background function and added settings to ain simulation loop

--- dataset/DataSimulator/augment_data.py
+++ dataset/DataSimulator/augment_data.py
@@ -1,11 +1,12 @@
 import os
 import cv2
+from PIL import Image
 import albumentations as A
 import numpy as np
 from tqdm import tqdm
 
 
-def create_card_directories(base_path):
+def _create_card_directories(base_path):
     print(base_path)
     # Define the ranks and suits
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -13,7 +14,6 @@ def create_card_directories(base_path):
 
     # Create the base directory if it doesn't exist
     if not os.path.exists(base_path):
-        print("nope")
         os.makedirs(base_path)
 
     # Loop through each rank and suit to create directories
@@ -27,37 +27,63 @@ def create_card_directories(base_path):
 
     print(f"Directories for all 52 cards have been created in {base_path}")
 
+def remove_backgrounds(input_dir):
+    for sub_dir in os.listdir(input_dir):
+        sub_dir_path = os.path.join(input_dir, sub_dir)
+
+        for filename in tqdm(os.listdir(sub_dir_path), desc=f"Removing bg's from {sub_dir}"):
+            if filename.endswith((".png", ".jpg", ".jpeg")):
+                img_path = os.path.join(sub_dir_path, filename)
+                img = Image.open(img_path).convert("RGBA")
+                datas = img.getdata()
+
+                new_data = []
+                for item in datas:
+                    # Change white (or near-white) to transparent
+                    if item[0] < 10 and item[1] < 10 and item[2] < 10:
+                        new_data.append((255, 255, 255, 0))
+                    else:
+                        new_data.append(item)
+
+                img.putdata(new_data)
+                img.save(os.path.join(sub_dir_path, filename.replace('.jpg', '.png')))
+
+
 # Path to the main directory
+def augment_labels(input_dir, output_dir):
+
+    _create_card_directories(output_dir)
+
+    transform = A.Compose([
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.2),
+        A.Rotate(limit=15, p=0.5),
+        A.RandomBrightnessContrast(p=0.3),
+        A.GaussNoise(p=0.2),
+        A.RandomScale(scale_limit=0.2, p=0.4),
+    ])
+
+    # Augment images in each subdirectory
+    for sub_dir in os.listdir(input_dir):
+        sub_dir_path = os.path.join(input_dir, sub_dir)
+        
+        for img_file in tqdm(os.listdir(sub_dir_path), desc=f"Processing {sub_dir}"):
+            img_path = os.path.join(sub_dir_path, img_file)
+            image = cv2.imread(img_path)
+            
+            if image is None:
+                continue  # Skip corrupted or non-image files
+            
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            
+            # Generate 10 augmented versions per image
+            for i in range(15):
+                augmented = transform(image=image)['image']
+                aug_filename = f"{os.path.splitext(img_file)[0]}_aug{i}.png"
+                aug_path = os.path.join(output_dir, sub_dir, aug_filename)
+                cv2.imwrite(aug_path, cv2.cvtColor(augmented, cv2.COLOR_RGB2BGR))
+
 input_dir = "dataset\\assets\cards\labels_default"
-output_dir = "dataset\\assets\cards\labels_augmented1"
-create_card_directories(output_dir)
-
-transform = A.Compose([
-    A.HorizontalFlip(p=0.5),
-    A.VerticalFlip(p=0.2),
-    A.Rotate(limit=30, p=0.5),
-    A.RandomBrightnessContrast(p=0.3),
-    A.GaussNoise(p=0.2),
-    A.RandomScale(scale_limit=0.2, p=0.4),
-])
-
-# Augment images in each subdirectory
-for sub_dir in os.listdir(input_dir):
-    sub_dir_path = os.path.join(input_dir, sub_dir)
-    
-    for img_file in tqdm(os.listdir(sub_dir_path), desc=f"Processing {sub_dir}"):
-        img_path = os.path.join(sub_dir_path, img_file)
-        image = cv2.imread(img_path)
-        
-        if image is None:
-            continue  # Skip corrupted or non-image files
-        
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        
-        # Generate 10 augmented versions per image
-        for i in range(15):
-            augmented = transform(image=image)['image']
-            aug_filename = f"{os.path.splitext(img_file)[0]}_aug{i}.png"
-            aug_path = os.path.join(output_dir, sub_dir, aug_filename)
-            print(aug_path)
-            cv2.imwrite(aug_path, cv2.cvtColor(augmented, cv2.COLOR_RGB2BGR))
+augmented_dir = "dataset\\assets\cards\labels_augmented"
+augment_labels(input_dir, augmented_dir)
+remove_backgrounds(augmented_dir)
--- dataset/DataSimulator/main.py
+++ dataset/DataSimulator/main.py
@@ -16,17 +16,15 @@ OUTPUT_LABELS.mkdir(parents=True, exist_ok=True)
 # Load card names
 card_names = sorted([f.stem for f in CARD_DIR.glob("*/")])
 card_to_class = {name: idx for idx, name in enumerate(card_names)}
-used_card_tracker = defaultdict(int)
+used_card_count = defaultdict(int)
 
 # Settings
 TARGET_USES_PER_CARD = 200
-CARDS_PER_IMAGE = 5
-MAX_TOTAL_IMAGES = (TARGET_USES_PER_CARD * len(card_names)) // CARDS_PER_IMAGE
 
 # Function to choose cards that need more appearances
 def sample_balanced_cards(n=5):
     # Sort cards by how underused they are (lowest usage first)
-    sorted_cards = sorted(card_names, key=lambda c: used_card_tracker[c])
+    sorted_cards = sorted(card_names, key=lambda c: used_card_count[c])
     selected = []
     
     attempts = 0
@@ -48,30 +46,48 @@ def get_random_augmented_card_image(card_name):
 
 # Main image generation loop
 image_id = 0
-while any(used_card_tracker[c] < TARGET_USES_PER_CARD for c in card_names):
+while any(used_card_count[c] < TARGET_USES_PER_CARD for c in card_names):
+    table = Image.open(TABLE_PATH).convert("RGBA")
+    # There can be 2, 5, 6, or 7 visible cards in a game state 
+    CARDS_PER_IMAGE = random.choice([2, 5, 6, 7])
+    MAX_TOTAL_IMAGES = (TARGET_USES_PER_CARD * len(card_names)) // CARDS_PER_IMAGE
+    W, H = table.size
+    CARD_W, CARD_H = 100, 130
+    BOARD_START = 80
+    BOARD_HEIGHT = 100
+    BOARD_GAP = CARD_W + 12
+    HOLE_CARD_START = W // 2 - 50
+    HOLE_CARD_HEIGHT = H - 150
+    HOLE_CARD_GAP = 75
+    labels = []
     selected_cards = sample_balanced_cards(CARDS_PER_IMAGE)
 
-    table = Image.open(TABLE_PATH).convert("RGBA")
-    W, H = table.size
-    labels = []
 
     for i, card_name in enumerate(selected_cards):
         # Get a random augmented image for the card
         card_img_path = get_random_augmented_card_image(card_name)
         card_img = Image.open(card_img_path).convert("RGBA")
-        card_img = card_img.resize((70, 100))
+        card_img = card_img.resize((CARD_W, CARD_H))
 
-        x = 100 + i * 90 + random.randint(-10, 10)
-        y = H // 2 - 50 + random.randint(-10, 10)
+        # Place hole cards first
+        if i <= 1:
+            x = HOLE_CARD_START + i * HOLE_CARD_GAP + random.randint(-5, 5)
+            y = HOLE_CARD_HEIGHT + random.randint(-10, 10)
+
+        else:
+            x = BOARD_START + (i-2) * BOARD_GAP + random.randint(-5, 5)
+            y = H // 2 - BOARD_HEIGHT + random.randint(-10, 10)
+
+        # Draw card images over table    
         table.alpha_composite(card_img, (x, y))
 
-        x_center = (x + 35) / W
-        y_center = (y + 50) / H
-        w = 70 / W
-        h = 100 / H
+        x_center = (x + (CARD_W / 2)) / W
+        y_center = (y + (CARD_H / 2)) / H
+        w = CARD_W / W
+        h = CARD_H / H
         labels.append(f"{card_to_class[card_name]} {x_center:.6f} {y_center:.6f} {w:.6f} {h:.6f}")
 
-        used_card_tracker[card_name] += 1
+        used_card_count[card_name] += 1
 
     # Save image and label
     img_path = OUTPUT_IMAGES / f"poker_{image_id}.jpg"
@@ -87,5 +103,5 @@ while any(used_card_tracker[c] < TARGET_USES_PER_CARD for c in card_names):
 
 # Print summary
 print("Final card usage:")
-for card, count in sorted(used_card_tracker.items()):
+for card, count in sorted(used_card_count.items()):
     print(f"{card}: {count}")
